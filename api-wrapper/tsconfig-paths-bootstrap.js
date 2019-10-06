418d8b172fa2eecd1761d567526f99121e2b7aac
ragauskl <ragauskl@gmail.com>
2019-10-06 17:17:46 +0100

    Removed file

--- api-wrapper/tsconfig-paths-bootstrap.js
+++
@@ -1,7 +0,0 @@
-const tsConfig = require('./tsconfig.json')
-const tsConfigPaths = require('tsconfig-paths')
-
-const cleanup = tsConfigPaths.register({
-  baseUrl: `dist`,
-  paths: tsConfig.compilerOptions.paths
-})
