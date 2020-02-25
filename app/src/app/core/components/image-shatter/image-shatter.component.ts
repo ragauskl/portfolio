d2f05457e31ed5af03708cfcd5b6ccad9ada6b2b
ragauskl <ragauskl@gmail.com>
2020-02-25 22:23:09 +0000

    Added test case for image split on multiple triangles on hover

--- app/src/app/core/components/image-shatter/image-shatter.component.ts
+++ app/src/app/core/components/image-shatter/image-shatter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef, AfterViewInit, OnDestroy, Input } from '@angular/core'
-import * as Three from 'three'
+import * as THREE from 'three'
 import { Subscription, fromEvent } from 'rxjs'
 
 @Component({
@@ -12,15 +12,16 @@ export class ImageShatterComponent implements AfterViewInit, OnDestroy {
 
   @Input() src: string
 
-  camera: Three.PerspectiveCamera
-  scene: Three.Scene
-  planeGeometry: Three.PlaneGeometry
-  material: Three.MeshNormalMaterial
-  mesh: Three.Mesh
-  renderer: Three.WebGLRenderer
+  camera: THREE.PerspectiveCamera
+  scene: THREE.Scene
+  planeGeometry: THREE.PlaneGeometry | THREE.PlaneBufferGeometry
+  bufferGeometry: THREE.BufferGeometry
+  material: THREE.MeshLambertMaterial | THREE.ShaderMaterial
+  mesh: THREE.Mesh
+  renderer: THREE.WebGLRenderer
 
-  raycaster: Three.Raycaster
-  rendererMouse: Three.Vector2
+  raycaster: THREE.Raycaster
+  rendererMouse: THREE.Vector2
 
   // Mouse position relative to element
   mouse = {
@@ -64,6 +65,7 @@ export class ImageShatterComponent implements AfterViewInit, OnDestroy {
         const event = e as MouseEvent
         // this.rotateMesh(0, 0)
         this.resetAnimation()
+        this.RenderImageAs('plane')
       })
     )
 
@@ -82,7 +84,8 @@ export class ImageShatterComponent implements AfterViewInit, OnDestroy {
         this.raycaster.setFromCamera(this.rendererMouse, this.camera)
 
         const [intersection] = this.raycaster.intersectObject(this.mesh)
-        if (intersection) console.log('Hover')
+        // if (intersection && !this.bufferGeometry) this.RenderImageAs('buffer')
+        this.RenderImageAs('buffer')
         this.rotateMesh()
       })
     )
@@ -107,43 +110,106 @@ export class ImageShatterComponent implements AfterViewInit, OnDestroy {
 
   init () {
     const { clientHeight, clientWidth } = this.element
-    this.camera = new Three.PerspectiveCamera(50, clientWidth / clientHeight, 0.01, 1000)
+    this.camera = new THREE.PerspectiveCamera(50, clientWidth / clientHeight, 0.01, 1000)
     this.camera.position.z = 100
 
-    this.scene = new Three.Scene()
-    const textureLoader = new Three.TextureLoader()
-    const material = new Three.MeshLambertMaterial({
-      map: textureLoader.load(this.src)
-    })
+    this.scene = new THREE.Scene()
 
-    this.planeGeometry = new Three.PlaneGeometry(70, 70 * 0.6)
-    this.material = new Three.MeshNormalMaterial()
-    // this.mesh = new Three.Mesh(this.geometry, this.material)
-    this.mesh = new Three.Mesh(this.planeGeometry, material)
-    this.mesh.position.set(0,0,0)
-
-    const light = new Three.PointLight(0xffffff, 1, 0)
+    const light = new THREE.PointLight(0xffffff, 1, 0)
     light.position.set(1, 1, 400)
 
-    window['geom'] = this.planeGeometry
-    console.log('this.planeGeometry.vertices:', this.planeGeometry.vertices)
-
-    this.scene.add(this.mesh)
     this.scene.add(light)
 
-    this.renderer = new Three.WebGLRenderer({ antialias: true })
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
     this.renderer.setClearColor('#6e6e6e') // 0xffffff
     this.renderer.setSize(clientWidth, clientHeight)
     this.element.appendChild(this.renderer.domElement)
 
-    this.raycaster = new Three.Raycaster()
-    this.rendererMouse = new Three.Vector2()
+    this.raycaster = new THREE.Raycaster()
+    this.rendererMouse = new THREE.Vector2()
 
-    this.updateRenderer()
+    this.RenderImageAs('plane')
 
     setTimeout(() => this.updateRenderer(), 500)
 
     window['obj'] = this
+  }
+
+  private RenderImageAs (geom?: 'plane' | 'buffer') {
+    if (this.mesh) {
+      this.scene.remove(this.mesh)
+      this.mesh = undefined
+      this.planeGeometry = undefined
+      this.bufferGeometry = undefined
+    }
+
+    if (!this.material) {
+      const vertexShader = document.getElementById('vertex-shader').textContent
+      const fragmentShader = document.getElementById('fragment-shader').textContent
+
+      const loader = new THREE.TextureLoader()
+      this.material = new THREE.ShaderMaterial({
+        uniforms: {
+          texture: {
+            type: 't',
+            value: loader.load(this.src)
+          }
+        },
+        vertexShader,
+        fragmentShader
+      })
+    }
+
+    const imageSize = { width: 70, height: 42 }
+    const coords = { x: -(imageSize.width / 2), y: -(imageSize.height / 2), z: 0 }
+
+    const setMesh = (geom: THREE.Geometry | THREE.BufferGeometry) => {
+      this.mesh = new THREE.Mesh(geom, this.material)
+      this.mesh.position.set(0,0,0)
+      this.scene.add(this.mesh)
+    }
+    if (geom === 'plane') {
+      this.planeGeometry = new THREE.PlaneBufferGeometry(imageSize.width, imageSize.height)
+      setMesh(this.planeGeometry)
+
+      // console.log('this.planeGeometry.vertices:', this.planeGeometry.vertices)
+    } else if (geom === 'buffer') {
+      this.bufferGeometry = new THREE.BufferGeometry()
+      const vertices = new Float32Array([
+        coords.x, coords.y, coords.z, // bottom left
+        coords.x + imageSize.width, coords.y, coords.z, // bottom right
+        coords.x + imageSize.width, coords.y + imageSize.height, coords.z, // upper right
+
+        coords.x + imageSize.width, coords.y + imageSize.height, coords.z + 10, // upper right
+        coords.x, coords.y + imageSize.height, coords.z + 10, // upper left
+        coords.x, coords.y, coords.z + 10 // bottom left
+      ])
+
+      // set the uvs for this box; these identify the following corners:
+      // lower-left, lower-right, upper-right, upper-left
+      const uvs = new Float32Array([
+        0.0, 0.0, // bottom left
+        1.0, 0.0, // bottom right
+        1.0, 1.0, // upper right
+
+        1.0, 1.0, // upper right
+        0.0, 1.0, // upper left
+        0.0, 0.0 // bottom left
+      ])
+
+      // indices = sequence of index positions in `vertices` to use as vertices
+      // we make two triangles but only use 4 distinct vertices in the object
+      // the second argument to THREE.BufferAttribute is the number of elements
+      // in the first argument per vertex
+      // this.bufferGeometry.setIndex([0,1,2, 3,4,5])
+      this.bufferGeometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
+      this.bufferGeometry.setAttribute('uv', new THREE.BufferAttribute(uvs, 2))
+
+      setMesh(this.bufferGeometry)
+
+    }
+
+    this.updateRenderer()
   }
 
   private _resetAnimation
