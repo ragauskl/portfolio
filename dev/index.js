4905e8e3edbb7e34ee0dc1d9b1b16e22fb57a583
ragauskl <ragauskl@gmail.com>
2020-04-24 21:45:51 +0100

    Sentry changed to env variable

--- dev/index.js
+++ dev/index.js
@@ -1,7 +1,7 @@
 const Sentry = require('@sentry/node')
 
 Sentry.init({
-  dsn: 'https://0c3a9f2696cb4dd8bd377bea95f1c98d@o205237.ingest.sentry.io/5212916'
+  dsn: process.env.SENTRY
 })
 
 Sentry.setContext('environment', process.env)
