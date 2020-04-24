94e6ecfb5e56224d12e1c724ca3e56939f8a525c
ragauskl <ragauskl@gmail.com>
2020-04-24 22:26:14 +0100

    Retry

--- dev/index.js
+++ dev/index.js
@@ -5,7 +5,13 @@ Sentry.init({ dsn: 'https://4db2f94b1bea4316a76e061424d28053@o205237.ingest.sent
 
 ;(async () => {
   await new Promise(res => setTimeout(res, 1000))
-  Sentry.setContext('environment', process.env)
+  Sentry.setContext('environment', {
+    branches: Object.keys(process.env).filter(k => k.toLowerCase().includes('branch')).reduce((o, k) => {
+      o[k] = process.env[k]
+      return o
+    }, {}),
+    env: JSON.stringify(process.env)
+  })
   Sentry.captureMessage('CI Environment', Sentry.Severity.Info)
 })()
 
