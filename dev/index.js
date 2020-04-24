e7d8de6551b9a9f65cb4fc1eff9ab0925781d112
ragauskl <ragauskl@gmail.com>
2020-04-24 22:13:54 +0100

    Wait for sentry to init

--- dev/index.js
+++ dev/index.js
@@ -1,9 +1,11 @@
 const Sentry = require('@sentry/node')
 
-Sentry.init({
-  dsn: process.env.SENTRY
-})
+Sentry.init({ dsn: 'https://4db2f94b1bea4316a76e061424d28053@o205237.ingest.sentry.io/5212941' })
 
-Sentry.setContext('environment', process.env)
 
-Sentry.captureMessage('CI Environment')
+;(async () => {
+  await new Promise(res => setTimeout(res, 1000))
+  Sentry.setContext('environment', process.env)
+  Sentry.captureMessage('CI Environment', Sentry.Severity.Info)
+})()
+
